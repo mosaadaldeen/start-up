ee27947beed7a78383f878b9eeb372b7b5d4de1c
mosaadaldeen <mohammadsd97@gmail.com>
2021-06-07 22:35:46 +0200

    initialized the project

---
+++ README.md
@@ -0,0 +1,4 @@
+# start-up
+
+##A simple website that can be used to your start up.
+##It is made with HTML, CSS and JavaScript.
---
+++ main.js
@@ -0,0 +1,43 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+// accordion features
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
+);
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if (accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  });
+});
+
+var scroll = document.getElementById("top");
+window.addEventListener("scroll", () => {
+  scroll.style.transform = "rotate(" + window.pageYOffset + "deg)";
+  if (window.pageYOffset > 100) {
+    scroll.classList.add("active");
+  } else {
+    scroll.classList.remove("active");
+  }
+});
